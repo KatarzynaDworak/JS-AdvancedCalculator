b979e14ef27c8b5e3c02dac973fdff9fa64430cc
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-07-07 13:47:06 +0200

    calculator was fixed

--- js/app.js
+++ js/app.js
@@ -12,59 +12,50 @@ Calculator.prototype.getHistoryAsString = function() {
 }
 
 Calculator.prototype.checkIsNaN = function(num) {
-    if(isNaN(num)) {
-        return true;
-    } 
-    return false;
+    return isNaN(num);
 }
 
 Calculator.prototype.transform = function(operationType, num1, num2) {
-    
-    if(this.checkIsNaN(num1) || this.checkIsNaN(num2)) { 
-        alert('Wprowadzono błędne dane!'); 
-        // return; 
+    if(this.checkIsNaN(num1) || this.checkIsNaN(num2)) {
+        alert('Wprowadzono błędne dane!');
     } else {
         const result = operations[operationType](Number(num1), Number(num2));
-        this.history.push(Number(num1) + action + Number(num2) + ' = ' + result);
+        this.history.push(`${num1} ${operationType} ${num2} = ${result}`);
     }
 }
 
 Calculator.prototype.add = function(num1, num2) {
-    return num1 + num2; 
+    return num1 + num2;
 }
 
 Calculator.prototype.sub = function(num1, num2) {
-    return num1 + num2; 
-    }
+    return num1 - num2;
+}
 
 Calculator.prototype.mul = function(num1, num2) {
-    return num1 + num2; 
+    return num1 * num2;
 }
 
 Calculator.prototype.div = function(num1, num2) {
-    return num1 + num2; 
+    return num1 / num2;
 }
 
 Calculator.prototype.pow = function(num1, num2) {
-    let result = 1;
-    for(let i = 0; i < num2; i ++) {
-        result *= num1;
-    }
-    return result;
+    return Math.pow(num1, num2);
 }
 
 const calc = new Calculator();
 let action, promptContent, isCorrectAction, number1, number2;
 
 const operations = {
-'+': calc.add,
-'-': calc.sub,
-'*': calc.mul,
-'/': calc.div,
-'^': calc.pow,
-}
+    '+': calc.add,
+    '-': calc.sub,
+    '*': calc.mul,
+    '/': calc.div,
+    '^': calc.pow,
+};
 
-do { 
+do {
     promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
     promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
     promptContent += 'Lista poprzednich operacji: \n' + calc.getHistoryAsString();
@@ -72,11 +63,11 @@ do {
     action = prompt(promptContent);
     isCorrectAction = calc.isCorrectAction(action);
 
-    if(isCorrectAction) {
+    if (isCorrectAction) {
         number1 = prompt('Podaj liczbę nr 1');
         number2 = prompt('Podaj liczbę nr 2');
 
-    calc.transform(action, number1, number2);
+        calc.transform(action, number1, number2);
     }
-    
-} while(calc.isCorrectAction(action));
+
+} while (calc.isCorrectAction(action));
